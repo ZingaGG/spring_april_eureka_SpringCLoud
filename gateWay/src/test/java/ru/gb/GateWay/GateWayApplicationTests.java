d886af8a8faa3379856e2241b0639af2619c736f
ZingaG <trad_bar@mail.ru>
2024-05-23 13:09:48 +0300

    Commit

---
+++ gateWay/src/test/java/ru/gb/GateWay/GateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.GateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
